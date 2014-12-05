451bb7d560fc1ab31b7194040ff1659f0ba4361a
12130010@st.hcmuaf.edu.vn <12130010@st.hcmuaf.edu.vn@8dbc3e88-1c7f-8be9-98ad-197aa4064837>
2014-12-05 07:45:27 +0000

    them class DuongDan.java

---
+++ src/util/DuongDan.java
@@ -0,0 +1,28 @@
+package util;
+
+public class DuongDan {
+	//
+	public static final String TRANG_CHU = "/BanVeXe/jsp/body/jsp";
+	public static final String GIOI_THIEU = "/BanVeXe/jsp/gioithieu.jsp";
+	public static final String THANH_TOAN = "/BanVeXe/jsp/thanhtoan.jsp";
+	public static final String LIEN_HE = "/BanVeXe/jsp/lienhe.jsp";
+	public static final String QUEN_MAT_KHAU = "/BanVeXe/jsp/quenmk.jsp";
+	public static final String DANG_KY = "/BanVeXe/jsp/DangKi.jsp";
+	public static final String DANG_NHAP = "/BanVeXe/jsp/DangNhap.jsp";
+	public static final String TIM_CHUYEN = "/BanVeXe/jsp/timchuyen.jsp";
+	public static final String CHI_TIET_DAT_VE = "/BanVeXe/jsp/chitietvexe.jsp";
+	public static final String FOOTER = "/BanVeXe/jsp/footer.jsp";
+	public static final String HEADER = "/BanVeXe/jsp/header.jsp";
+	public static final String XE_16 = "/BanVeXe/jsp/xe16.jsp";
+	public static final String XE_45 = "/BanVeXe/jsp/xe45.jsp";
+	public static final String TIN_TUC = "/BanVeXe/jsp/tintuc.jsp";
+	public static final String KIEM_TRA_THONG_TIN = "/BanVeXe/jsp/kiemtrave.jsp";
+	public static final String DAT_VE_DI = "/BanVeXe/jsp/datvedi.jsp";
+	public static final String DAT_VE_VE = "/BanVeXe/jsp/datveve.jsp";
+	public static final String CHI_TIET_VE = "/BanVeXe/jsp/chitietve.jsp";
+	//
+	public static final String TIM_GHE = "/BanVeXe/TimGhe";
+	public static final String TIM_TUYEN = "/BanVeXe/TimTuyen";
+	public static final String CAPTCHA = "/BanVeXe/GenerateCaptcha";
+	
+}
